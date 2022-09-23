9b37d58200fc4e4a474eadfc7f5fa305fac6839a
Natalya Terebova <natalyaterebova@Natalyas-MacBook-Pro.local>
2022-09-23 17:37:18 +0300

    lesson5 add functions

---
+++ func.js
@@ -0,0 +1,21 @@
+let str = "hello";
+let boolean = false;
+console.log(str + boolean);
+let num = 3;
+console.log(str + num);
+console.log(num + boolean);
+
+console.log(str * boolean);
+console.log(str * num);
+console.log(num * boolean);
+
+console.log(str / boolean);
+console.log(str / num);
+console.log(num / boolean);
+
+console.log(Number(boolean));
+console.log(typeof Number(boolean));
+console.log(String(num));
+console.log(typeof String(num));
+console.log(Boolean(num));
+console.log(typeof Boolean(num));
